a15c73d1e47604001fa11200faa45961b3628093
Alan Robertson <alan@qrop.xyz>
2024-12-16 09:41:15 +1100

    Setup added and renamed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='lattice_surgery_draw',
+    packages=['lattice_surgery_draw'],
+    package_dir={
+        '':'src'},
+    include_package_data=True,
+)
